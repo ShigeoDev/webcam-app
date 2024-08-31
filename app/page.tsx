1e362ee52d76b7f4f46940629f0b556a399ad141
Nigel <84653162+ShigeoDev@users.noreply.github.com>
2024-08-31 00:07:51 -0700

    basis

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,52 @@
-import Image from "next/image";
+'use client'
+
+import { useRef } from 'react'
+import * as tf from "@tensorflow/tfjs"
+import * as handpose from "@tensorflow-models/handpose"
+import Webcam from "react-webcam"
 
 export default function Home() {
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log('handpose');
+
+  }
+
+  const detect = async (net: any) => {
+    if (
+      webcamRef.current &&
+      webcamRef.current.video?.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      if (canvasRef.current) {
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+      }
+
+      const hand = await net.estimateHands(video);
+      console.log(hand);
+    }
+
+
+
+  }
+
+  runHandpose();
+
   return (
-    <div>Page</div>
+    <div>
+      <Webcam ref={webcamRef} className="w-screen h-screen" />
+      <canvas ref={canvasRef} className="w-screen hscreen" />
+    </div>
   );
 }
